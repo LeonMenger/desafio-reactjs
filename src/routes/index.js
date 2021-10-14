044ad9935721dad89eea920f4af3619a7e630194
Leon Menger <leon.menger@elastacloud.com>
2021-10-14 17:50:48 -0300

    feat: add paths file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "../pages/Home";
+import { PATHS } from "./paths";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={Home} exact />
+                <Route path={PATHS.home} component={Home} exact />
             </Switch>
         </BrowserRouter>
     );
