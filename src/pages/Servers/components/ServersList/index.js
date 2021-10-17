f2257d88a0d7b03b01257bdb561a8ea6fd96c3d7
Leon Menger <leon.menger@elastacloud.com>
2021-10-17 19:23:56 -0300

    feat: add loading feature on data table

--- src/pages/Servers/components/ServersList/index.js
+++ src/pages/Servers/components/ServersList/index.js
@@ -1,72 +1,77 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Card from "../../../../components/Card";
+import useAxios from "../../../../hooks/useAxios";
 import useSortableData from "../../../../hooks/useSortableData";
-import api from "../../../../services/api";
 import ServerItem from "../ServerItem";
+import { PATHS } from "../../../../routes/paths";
 import "./styles.css";
 
 const ServersList = () => {
+    const { response, loading, error } = useAxios(PATHS.servers);
     const { sortedData, setSortedData, sortBy } = useSortableData();
 
     useEffect(() => {
-        api.get("servers")
-            .then(({ data }) => {
-                setSortedData(data);
-            })
-            .catch((err) => {
-                console.error("Erro: " + err);
-            });
-    }, []);
+        if (response !== null) {
+            setSortedData(response);
+        }
+    }, [response]);
+
+    const tableContent = (
+        <table className="list-table">
+            <thead>
+                <tr>
+                    <th>Select</th>
+                    <th
+                        onClick={() => {
+                            sortBy("hostname");
+                        }}
+                    >
+                        Hostname
+                    </th>
+                    <th
+                        onClick={() => {
+                            sortBy("configuracao.memoryProvisioned");
+                        }}
+                    >
+                        Memória
+                    </th>
+                    <th
+                        onClick={() => {
+                            sortBy("configuracao.cpuProvisioned");
+                        }}
+                    >
+                        vCPUs
+                    </th>
+                    <th
+                        onClick={() => {
+                            sortBy("configuracao.totalDiskGB");
+                        }}
+                    >
+                        Disco
+                    </th>
+                    <th
+                        onClick={() => {
+                            sortBy("ip");
+                        }}
+                    >
+                        IP
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                {sortedData?.map((server) => (
+                    <ServerItem key={server.id_vm} server={server} />
+                ))}
+            </tbody>
+        </table>
+    );
+
+    const errorContent = <p className="error">Ops! Ocorreu um erro.</p>;
+    const loadingContent = <p className="loading">Loading</p>;
 
     return (
         <Card title="Tabela de servidores">
-            <table className="list-table">
-                <thead>
-                    <tr>
-                        <th>Select</th>
-                        <th
-                            onClick={() => {
-                                sortBy("hostname");
-                            }}
-                        >
-                            Hostname
-                        </th>
-                        <th
-                            onClick={() => {
-                                sortBy("configuracao.memoryProvisioned");
-                            }}
-                        >
-                            Memória
-                        </th>
-                        <th
-                            onClick={() => {
-                                sortBy("configuracao.cpuProvisioned");
-                            }}
-                        >
-                            vCPUs
-                        </th>
-                        <th
-                            onClick={() => {
-                                sortBy("configuracao.totalDiskGB");
-                            }}
-                        >
-                            Disco
-                        </th>
-                        <th
-                            onClick={() => {
-                                sortBy("ip");
-                            }}
-                        >
-                            IP
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {sortedData?.map((server) => (
-                        <ServerItem key={server.id_vm} server={server} />
-                    ))}
-                </tbody>
-            </table>
+            {loading ? loadingContent : error ? errorContent : tableContent}
         </Card>
     );
 };
